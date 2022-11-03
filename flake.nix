3aa43091681af3e74eeca5c76add8be17cd2f468
Mr. Fluffy <git@sixearsm>
2022-11-03 05:48:17 +0000

    base0->0.0.4.2;base0t->0.0.1.2

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.4";
+    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.5";
 
-    base1.url                = "github:sixears/base1/r0.0.9.5";
-    more-unicode.url         = "github:sixears/more-unicode/r0.0.17.3";
-    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.6";
-    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.5";
-    textual-plus.url         = "github:sixears/textual-plus/r1.0.2.5";
+    base1.url                = "github:sixears/base1/r0.0.9.6";
+    more-unicode.url         = "github:sixears/more-unicode/r0.0.17.4";
+    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.7";
+    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.6";
+    textual-plus.url         = "github:sixears/textual-plus/r1.0.2.6";
   };
 
   outputs = { self, nixpkgs, flake-utils, build-utils
